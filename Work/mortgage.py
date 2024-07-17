d1f347b740224c0eea0afc99f8423fb27c451a8b
souravoo7 <soumonsou@gmail.com>
2024-07-18 03:36:58 +0530

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,14 @@
-# mortgage.py
-#
-# Exercise 1.7
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 19:37:04 2024
+
+@author: sourav
+
+#Run the below command before saving scripts
+
+import os
+
+os.chdir("C:/Users/soura/Documents/Python Scripts/python_referesher")
+
+"""
+
